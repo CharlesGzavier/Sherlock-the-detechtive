7fda8a19e57e1ba10319881f1ce44364f66f1630
CharlesGzavier <82609530+CharlesGzavier@users.noreply.github.com>
2021-09-01 08:24:45 +0200

    V2
    this is an update after changing the settings on Airship

--- push-worker.js
+++ push-worker.js
@@ -1,9 +1,9 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1630259967.1586192
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1630477231.961453
 importScripts('https://aswpsdkeu.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://img.icons8.com/ios/50/000000/centralized\u002Dnetwork.png',
   defaultTitle: 'Carlos Brand Name ',
-  defaultActionURL: 'https://charlesgzavier.github.io/cv/',
+  defaultActionURL: 'https://charlesgzavier.github.io/Sherlock\u002Dthe\u002Ddetechtive/',
   appKey: 'Vm0ghpyrThqyjaCf5NV4cg',
   token: 'MTpWbTBnaHB5clRocXlqYUNmNU5WNGNnOlhXUHk4dzNHa2k2Mnk1M2U1bTI4aF9SMnVqNTU5MHU4NG94YlBGOFA2aFU',
   vapidPublicKey: 'BLJpsCE9cWW4MzKEXMBQ3P1YRM_g_kF6k7LQrQUrMds0PAUqFb9JTfDIP2E1tiYtlSSRXAMudY6achuKqr3x3Yk='
